b8783c66a4d429bcb68ea6bc44f32db7374ffb9f
Hitesh Jambhulkar <hiteshj1306@gmail.com>
2020-05-18 17:03:58 +0530

    Preparing deployment

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const port = process.env.PORT || 8000;
 
 // db
 mongoose
-  .connect(process.env.DATABASE, {
+  .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useCreateIndex: true,
   })
