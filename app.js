fe841fb2c8a0ff5eb1b654f8f0a6501b52a31955
Hitesh Jambhulkar <hiteshj1306@gmail.com>
2020-05-18 23:54:35 +0530

    Preparing for deployment

--- app.js
+++ app.js
@@ -45,7 +45,6 @@ app.use('/api', orderRoutes);
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
-
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
   });
